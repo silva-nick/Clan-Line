8dfa1754caff2a35eafc03aec664f3c4bd0342df
Nick Silva <silva.d.nicholas@gmail.com>
2018-07-18 12:10:41 -0500

    Importing Project

--- app/src/main/java/com/test/nick/soccerapp/GameView.java
+++ app/src/main/java/com/test/nick/soccerapp/GameView.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.util.Log;
 import android.view.SurfaceView;
 import android.view.SurfaceHolder;
 
